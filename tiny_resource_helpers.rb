1e6c4c7d3854b49675e2c4912be732562bb77e83
ippa <iplord@iplord.com>
2010-11-20 09:45:55 +0100

    various fixes of path/url helpers

--- tiny_resource_helpers.rb
+++ tiny_resource_helpers.rb
@@ -13,27 +13,27 @@ module TinyResourceHelpers
   end
 
   # => /projects/5
-  def resource_url(resource = nil)
-    url_for(resource || self.resource)
+  def resource_url(resource = self.resource)
+    url_for(:controller => controller_path, :action => "show", :id => resource)
   end
-  def resource_path(resource = nil)
-    url_for(resource || self.resource)
+  def resource_path(resource = self.resource)
+    url_for(:controller => controller_path, :action => "show", :id => resource, :only_path => true)
   end
 
   # => /projects/5/edit
-  def edit_resource_url(resource = nil)
-    # [:edit, resource] ?!? se http://www.railsdispatch.com/
-    url_for(:controller => collection_name, :action => "edit", :id => (resource || self.resource.id))
+  def edit_resource_url(resource = self.resource)
+    url_for(:controller => controller_path, :action => "edit", :id => resource)
   end
-  def edit_resource_path(resource = nil)
-    url_for(:controller => collection_name, :action => "edit", :id => (resource || self.resource.id), :only_path => true)
+
+  def edit_resource_path(resource = self.resource)
+    url_for(:controller => controller_path, :action => "edit", :id => resource, :only_path => true)
   end
 
   # => /projects/new
-  def new_resource_url
+  def new_resource_url(object = nil)
     url_for(:controller => collection_name, :action => "new")
   end
-  def new_resource_path
+  def new_resource_path(object = nil)
     url_for(:controller => collection_name, :action => "new", :only_path => true)
   end
 
@@ -56,7 +56,19 @@ module TinyResourceHelpers
   def resource_name
     self.model.to_s.underscore
   end
-
+  
+  #
+  # An array of namespaces under which the current controller is.
+  # For example, in Admin::Content::PagesController:
+  #
+  # namespaces #=> [:admin, :content]
+  #
+  # From make_resourceful by hcatlin
+  #
+  #def namespaces
+  #  @namespaces ||= self.class.name.split('::').slice(0...-1).map(&:underscore).map(&:to_sym)
+  #end
+  
   # ProjectsController will return "projects"
   def collection_name
     self.model.to_s.tableize
